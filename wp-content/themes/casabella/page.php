c1b8fbb949030acd8e0e9bd330101bbb68229360
jluisj7 <jluisjab7@gmail.com>
2015-04-08 16:59:24 -0500

    page en un 50%

--- wp-content/themes/casabella/page.php
+++ wp-content/themes/casabella/page.php
@@ -82,8 +82,8 @@
     </div>
 
   <div class="row">
-    <div class="small-12 large-4 columns">
-      <div id="left">
+    <div class="small-12 medium-4 large-4 columns panel">
+      <div id="left" class="hide-for-small">
 
   <div class="gcBookingGadget vertical"></div>
 
@@ -189,7 +189,7 @@
 
 <div class="homenoticias">
 <h2 class="widgettitle">Contact Us</h2>
-<div class="textwidget">
+<div class="textwidget font-10">
 <span class="contact_widget_name">Casa Bella B&B Boutique Hotel</span><br /><span class="contact_widget_address">Las Flores 459 San Isidro </span><br /><span class="contact_widget_city">Lima – Peru</span>&nbsp;<br />
 
 <span class="contact_widget_phone">Phone in Peru: (511) 421-7354 - (511) 421-1033</span><br />
@@ -216,7 +216,8 @@
 
 </div>
     </div>
-    <div class="small-12 large-8 columns">
+    <div class="small-12 medium-8 large-8 columns text-content">
+      <div class="dots"></div>
       <?php if(have_posts()) : while(have_posts()) : the_post(); ?> 
 <?php the_content(); ?>
 <?php endwhile; ?> 
@@ -225,35 +226,39 @@
 <?php endif; ?>
     </div>
   </div>
-   <div class="row" style=" padding: 2em; margin-top: 1.5em;">
-          
-            <div class="small-12 small-centered columns">
-              
-              <ul class="small-block-grid-1 medium-block-grid-3 large-block-grid-5">
-  <li><a href="http://twitter.com/casabellaperu" title="hotel casa bella in twitter">
-                <img src="http://www.casabellaperu.net/wp-content/themes/casaweb/images/social/twitter.png"  alt="casa bella twitter" class="mb">
-              </a><!-- Your content goes here --></li>
-  <li><a href="http://www.facebook.com/casabellaperu" title="hotel casa bella in facebook">
-                <img src="http://www.casabellaperu.net/wp-content/themes/casaweb/images/social/facebook.png" alt="casa bella facebook" class="mb">
-              </a><!-- Your content goes here --></li>
-  <li><a href="http://www.tripadvisor.com/Hotel_Review-g294316-d628608-Reviews-Casa_Bella_B_B-Lima.html" title="hotel casa bella in tripadvisor">
-                <img src="http://www.casabellaperu.net/wp-content/themes/casaweb/images/social/tripadvisor.png" alt="casa bella tripadvisor" class="mb">
-              </a><!-- Your content goes here --></li>
-  
-<li><a href="http://www.peru.info/es/iperu-oficinas-de-informacion-y-asistencia-al-turista-ip4-1-home" title="Tourist information and assistance">
-                <img src="<?php bloginfo( 'template_directory' ); ?>/images/social/iperu.jpg" alt="Tourist information and assistance" style="margin-right:0" class="mb">
-              </a></li>
-              <li><a href="#">
-                <img width="84" height="38" border="0" alt="El Tiempo Lima-Callao / Aerop. Internacional Jorgechavez" src="http://www.tutiempo.net/imagenes_asociados/84x38/SPIM.png" style="" class="mb">
-              </a></li>
-</ul>
-              
-              
-              
-          
 
-            </div>         
-      </div>
+   <div class="border-top">
+     <div class="row" style=" padding: 2em; margin-top: 1.5em;">
+            
+              <div class="small-12 small-centered columns">
+                
+                <ul class="small-block-grid-1 medium-block-grid-3 large-block-grid-5">
+       <li><a href="http://twitter.com/casabellaperu" title="hotel casa bella in twitter">
+                  <img src="http://www.casabellaperu.net/wp-content/themes/casaweb/images/social/twitter.png"  alt="casa bella twitter" class="mb">
+                </a><!-- Your content goes here --></li>
+       <li><a href="http://www.facebook.com/casabellaperu" title="hotel casa bella in facebook">
+                  <img src="http://www.casabellaperu.net/wp-content/themes/casaweb/images/social/facebook.png" alt="casa bella facebook" class="mb">
+                </a><!-- Your content goes here --></li>
+       <li><a href="http://www.tripadvisor.com/Hotel_Review-g294316-d628608-Reviews-Casa_Bella_B_B-Lima.html" title="hotel casa bella in tripadvisor">
+                  <img src="http://www.casabellaperu.net/wp-content/themes/casaweb/images/social/tripadvisor.png" alt="casa bella tripadvisor" class="mb">
+                </a><!-- Your content goes here --></li>
+       
+     <li><a href="http://www.peru.info/es/iperu-oficinas-de-informacion-y-asistencia-al-turista-ip4-1-home" title="Tourist information and assistance">
+                  <img src="<?php bloginfo( 'template_directory' ); ?>/images/social/iperu.jpg" alt="Tourist information and assistance" style="margin-right:0" class="mb">
+                </a></li>
+                <li><a href="#">
+                  <img width="84" height="38" border="0" alt="El Tiempo Lima-Callao / Aerop. Internacional Jorgechavez" src="http://www.tutiempo.net/imagenes_asociados/84x38/SPIM.png" style="" class="mb">
+                </a></li>
+     </ul>
+                
+                
+                
+            
+     
+              </div>         
+        </div>
+   </div>
+
       <div class="row" style="  margin-bottom: 1.5em;">
         <div class="small-6 medium-6 large-2 small-centered columns center">
         <img width="110" height="65" border="0" alt="marca peru" src="http://www.casabellaperu.net/images/marca-peru.jpg">
