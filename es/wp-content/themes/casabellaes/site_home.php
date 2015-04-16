061917360ba7d4d34435518727c3afb2a8e7aeef
jluisj7 <jluisjab7@gmail.com>
2015-04-16 13:12:21 -0500

    modificacion del cb/es

--- es/wp-content/themes/casabellaes/site_home.php
+++ es/wp-content/themes/casabellaes/site_home.php
@@ -34,7 +34,7 @@ Template Name: Home Page
     <div class="row bg-datos">
   <div class="small-12 columns datos">
     <p style="display:inline-block!important;">
-        <span class="hide-for-small" >Boutique Hotels Lima Perú - Av. De La Aviación 565 Miraflores Lima - Perú Teléfono: (511) 421-7354 - (511) 421 a 1.033 - En EE.UU.: (720) 648-3451</span>
+        <span class="hide-for-small" >Hoteles Boutique en Lima Perú - Av. De La Aviación 565 Miraflores Lima - Perú Phone: (511) 421-7354 - (511) 421-1033 - In USA: (720) 648-3451</span>
        <span class="flags"><a href="http://www.casabellaperu.net/casabella-es">
         <img src="http://www.casabellaperu.net/wp-content/themes/casaweb/images/icons/spanish.gif" width="16" height="11" alt="spanish" /></a> <a href="http://www.casabellaperu.net/"><img src="http://www.casabellaperu.net/wp-content/themes/casaweb/images/icons/english.gif" width="16" height="11" alt="english" /></a> <a href="http://www.peru.o-kom.info/casabellaperu/" target="_self"><img src="http://www.casabellaperu.net//wp-content/themes/casaweb/images/icons/german.gif" width="16" height="11" alt="german" /></a></span>
      </p>
@@ -66,19 +66,7 @@ Template Name: Home Page
   <section class="top-bar-section">
     <!-- Right Nav Section -->
     <?php wp_nav_menu(array('theme_location' => 'menu','menu_id' => 'menu', 'menu_class' => 'eleven columns','container' => '','items_wrap'      => '<ul class="left">%3$s</ul>',)); ?>
-    <ul class="right">      
-      <li class="has-dropdown">
-        <a href="#">Search Hotel</a>
-        <ul class="dropdown">
-          <li>
-              <a href="http://www.casabellaperu.net/miraflores/">Casa Bella San Miraflores</a>
-            </li>
-            <li>
-              <a href="http://www.casabellaperu.net/grand-reserve/">Casa Bella Miraflores Grand Reserve</a>
-            </li>         
-        </ul>
-      </li>
-    </ul>
+    
   </section>
 </nav>
 </div>
@@ -115,7 +103,7 @@ Template Name: Home Page
 	<div class="small-12 medium-6 large-4 columns margin-b">
 		<div class="botones">
 			<a href="<?php echo get_option('home'); ?>/airport-transfer-services/" title="<?php echo get_option('subbox1desc'); ?>">
-				<img src="<?php bloginfo('template_directory'); ?>/images/buttons/Airport-Transfer-service.png" alt="" class="btn_image">
+				<img src="<?php bloginfo('template_directory'); ?>/images/buttons/aeropuerto.png" alt="" class="btn_image">
 			</a>
 		</div>
 	</div>
@@ -123,7 +111,7 @@ Template Name: Home Page
 
 		<div class="botones">
 			<a href="<?php echo get_option('home'); ?>/rates-availability/" title="<?php echo get_option('subbox2desc'); ?>">
-				<img src="<?php bloginfo('template_directory'); ?>/images/buttons/Rates-&-Availability.png" alt="" class="btn_image">
+				<img src="<?php bloginfo('template_directory'); ?>/images/buttons/precios.png" alt="" class="btn_image">
 			</a>
 		</div>
 
@@ -132,7 +120,7 @@ Template Name: Home Page
 
 		<div class="botones">
 			<a href="https://secure.guestcentric.net/api/bg/book.php?apikey=f574d237ca9871f55c306a1903e7c50a" title="<?php echo get_option('subbox3desc'); ?>">
-				<img src="<?php bloginfo('template_directory'); ?>/images/buttons/Make-a-Reservation.png" alt="" class="btn_image"> 
+				<img src="<?php bloginfo('template_directory'); ?>/images/buttons/reserva.png" alt="" class="btn_image"> 
 			</a>
 		</div>
 
@@ -151,37 +139,49 @@ Template Name: Home Page
 		endwhile;
 		?>
 		<p>
-			<a href="<?php echo get_option('home'); ?>/about-us/">Read More &rarr;</a>
+			<a href="<?php echo get_option('home'); ?>/about-us/">Leer Mas &rarr;</a>
 		</p>
 		<div style="text-align:left; font-size:14px; color:#000; margin-bottom:20px;">
-			<p>Local Time: <span class="jclock"></span></p>
+			<p>Hora Local: <span class="jclock"></span></p>
 		</div>
 		<a href="http://livechat.boldchat.com/aid/238468884419305815/bc.chat?cwdid=2772812126678393419"><img width="307" height="115" border="0" src="<?php bloginfo('template_directory'); ?>/images/buttoms/livechat.jpg" alt="Live chat"></a>
 	</div>
 	<div class="small-12  medium-6 large-4 columns">
-		<?php 
-			query_posts('showposts=1&page_id=397');
-			while (have_posts()) : the_post();
-		?>
-		<h5 class="tit_home"><a href="<?php echo get_option('home'); ?>/category/room/"><?php the_title(); ?></a></h5>
-		<?php $more = 0; the_content("Read More &rarr;");
-		endwhile;
-		?>
-		<p><a href="<?php echo get_option('home'); ?>/?cat=10">View all rooms &rarr;</a></p>
-		<?php query_posts('showposts=1&page_id=954');
+		<?php query_posts('showposts=1&page_id=397');
 		while (have_posts()) : the_post(); ?>
+
+		<h5 class="tit_home"><a href="<?php echo get_option('home'); ?>/?cat=10"><?php the_title(); ?></a></h5>
+
+		<?php
+
+		$more = 0;
+
+		the_content("Leer Mas &rarr;");
+
+		endwhile; 
+
+		?>
+		<p><a href="<?php echo get_option('home'); ?>/?cat=10">Ver todas las habitaciones &rarr;</a></p>
+		<?php query_posts('showposts=1&page_id=764');
+		while (have_posts()) : the_post(); ?>
+
 		<h5 class="tit_home"><a href="<?php echo get_option('home'); ?>/tours/"><?php the_title(); ?></a></h5>
-		<?php $more = 0; the_content("Read More &rarr;");
-		endwhile;
+
+		<?php
+
+		$more = 0;
+
+		the_content("Leer Mas &rarr;");
+
+		endwhile; 
+
 		?>
 
 		<?php /*?><h5 class="tit_home"><a href="<?php echo get_option('home'); ?>/tours/">Lima City Tours</a></h5>
 		<p>A three-hour sightseeing tour of the colonial and modern Lima.</p>
 		<p><img src="<?php bloginfo('template_directory'); ?>/images/catedral-lima.jpg" width="297" height="118" alt="catedral lima" /></p>
 		<p>We will drive you through historical monuments and locations such as the Government Palace, the City Hall and the Cathedral of Lima at the Plaza Mayor.</p><?php */?>
-		<h5 class="tit_home">
-			<a href="<?php echo get_option('home'); ?>/category/blog/">News &amp; Blog</a>
-		</h5>
+		<h5 class="tit_home"><a href="<?php echo get_option('home'); ?>/category/blog/">Noticias &amp; Blog</a></h5>
 		<ul class="recent_post">
 			<?php query_posts('cat=-'.get_option('portfoliocat').',-'.get_option('featuredcat').'&showposts=5');
 			while (have_posts()) : the_post();?>
@@ -193,11 +193,17 @@ Template Name: Home Page
 	</div>
 	<div class="small-12  medium-6 large-4 columns">
 		<div id="blog_home">
-			<?php query_posts('showposts=1&page_id=956');
-			while (have_posts()) : the_post(); ?>
-			<h5 class="tit_home"><a href="<?php echo get_option('home'); ?>/deluxe-condo-suite-apartment/"><?php the_title(); ?></a></h5>
-			<?php $more = 0; the_content("Read More &rarr;");
-			endwhile;
+			<?php query_posts('showposts=1&page_id=767');
+				while (have_posts()) : the_post(); ?>
+				<h5 class="tit_home"><a href="<?php echo get_option('home'); ?>/contacto/"><?php the_title(); ?></a></h5>
+				<?php
+
+				$more = 0;
+
+				the_content("Leer Mas &rarr;");
+
+				endwhile; 
+
 			?>
 			<?php query_posts('showposts=1&page_id=462');
 			while (have_posts()) : the_post(); ?>
@@ -209,25 +215,27 @@ Template Name: Home Page
 			the_content("Read More &rarr;");
 			endwhile; 
 			?>
-			<h5 class="tit_home"><a href="http://www.casabellaperu.net/gallery/">Photo Tour</a></h5>
-			<?php query_posts('showposts=1&page_id=960');
-			while (have_posts()) : the_post();
+
+			<?php query_posts('showposts=1&page_id=770');
+			while (have_posts()) : the_post(); ?>
+
+			<h5 class="tit_home"><a href="http://www.casabellaperu.net/gallery/"><?php the_title(); ?></h5>
+
+			<?php
+
 			$more = 0;
-			the_content("Read More &rarr;");
-			endwhile;
-			?>
-			<?php /*?><h5 class="tit_home">Corporate Rates</h5>
-			<?php query_posts('showposts=1&page_id=962');
-			while (have_posts()) : the_post();
-			$more = 0;
-			the_content("Read More &rarr;");
+
+			the_content("Leer Mas &rarr;");
+
 			endwhile; 
-			?><?php */?>
+
+			?>
 			<iframe src="http://www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2Fpages%2FCasa-Bella-Boutique-Hotel%2F117910828286223&amp;width=292&amp;colorscheme=light&amp;show_faces=true&amp;stream=false&amp;header=true&amp;height=360" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:100%; height:345px;" allowTransparency="true"></iframe>
 			<div id="twitter">
 				<?php if (function_exists('dynamic_sidebar') && dynamic_sidebar(3) ) : else : ?>
 				<?php endif; ?>
 			</div>
+
 		</div>
 	</div>
 </div>
